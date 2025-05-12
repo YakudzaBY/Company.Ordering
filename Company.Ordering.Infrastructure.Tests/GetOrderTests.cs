59b952a552e65b0b91e544dce41bdf150b3aa73e
Vitaly Yakutik <4yakudz@gmail.com>
2025-05-12 12:31:57 +0200

    Add GetOrder tests

---
+++ Company.Ordering.Infrastructure.Tests/GetOrderTests.cs
@@ -0,0 +1,41 @@
+﻿using Company.Ordering.Infrastructure.Repositories;
+
+namespace Company.Ordering.Infrastructure.Tests;
+
+public class GetOrderTests : InMemoryDbTest
+{
+    private readonly Domain.OrderAggregate.Order _order = new()
+    {
+        InvoiceEmailAddress = "someone@example.com"
+    };
+
+    private readonly OrdersRepository _ordersRepository;
+
+    public GetOrderTests(): base()
+    {
+        _dbContext.Orders.Add(_order);
+        _dbContext.SaveChanges();
+
+        _ordersRepository = new OrdersRepository(_dbContext);
+    }
+
+    [Fact]
+    public async Task PositiveTestAsync()
+    {
+        //Act
+        var dbOrder = await _ordersRepository.GetOrderAsync(_order.Number);
+
+        //Assert
+        Assert.NotNull(dbOrder);
+    }
+
+    [Fact]
+    public async Task NegativeTestAsync()
+    {
+        //Act
+        var dbOrder = await _ordersRepository.GetOrderAsync(-1);
+
+        //Assert
+        Assert.Null(dbOrder);
+    }
+}
