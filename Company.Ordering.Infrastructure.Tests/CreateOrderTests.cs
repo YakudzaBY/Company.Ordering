af7d4ce44060122f9ffc17b22cb14b18b92a8daa
Vitaly Yakutik <4yakudz@gmail.com>
2025-05-15 16:58:39 +0200

    Add CreateOrderWithSameProductsAsync Tests

--- Company.Ordering.Infrastructure.Tests/CreateOrderTests.cs
+++ Company.Ordering.Infrastructure.Tests/CreateOrderTests.cs
@@ -10,7 +10,7 @@ public class CreateOrderTests: InMemoryDbTest
     [Theory]
     [InlineData(1)]
     [InlineData(2)]
-    public async Task TestAsync(int amount)
+    public async Task CreateOrderWithoutProductsAsync(int amount)
     {
         //Arrange
         var orders = new Order[amount];
@@ -30,6 +30,39 @@ public class CreateOrderTests: InMemoryDbTest
 
         //Assert
         foreach(var order in orders)
+        {
+            var orderFromDb = await _dbContext.Orders
+                .SingleOrDefaultAsync(o => o.OrderNumber == order.OrderNumber, CancellationToken.None);
+
+            Assert.NotNull(orderFromDb);
+        }
+    }
+
+    [Theory]
+    [InlineData(1)]
+    [InlineData(2)]
+    public async Task CreateOrderWithSameProductsAsync(int amount)
+    {
+        //Arrange
+        var orders = new Order[amount];
+        for (var i = 0; i < amount; i++)
+        {
+            var order = orders[i] = new Order(default, default, "asd@example.com", default, default);
+            await order.AddProductAsync(12345, default, 1, default);
+            await order.AddProductAsync(12345, default, 1, default);
+        }
+
+        var repo = new OrdersRepository(_dbContext);
+
+        //Act
+        foreach (var order in orders)
+        {
+            await repo.CreateOrderAsync(order, CancellationToken.None);
+        }
+        await repo.UnitOfWork.SaveChangesAsync(CancellationToken.None);
+
+        //Assert
+        foreach (var order in orders)
         {
             var orderFromDb = await _dbContext.Orders
                 .SingleOrDefaultAsync(o => o.OrderNumber == order.OrderNumber, CancellationToken.None);
